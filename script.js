fc4eec1304b3a95110c34571360d22ab243aa35a
Jennah <contactjennah@gmail.com>
2018-03-01 13:35:23 +0000

    Create functioning modal for form submission relates #10

--- script.js
+++ script.js
@@ -1,12 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Write your code below only:
-  const submit = document.getElementById("send");
+  // global variables 
+  var modal = document.querySelector("#modal");
+  var overlay = document.querySelector("#overlay");
+  var previouslyFocusedEl;
 
-  submit.addEventListener("click", alertMessageSubmit, false);
+  const submit = document.querySelector("#submit");
+  const send = document.querySelector("#send");
 
-  // Don't delete below this comment. Wrtie your code above!
+  send.addEventListener("click", function() {
+    event.preventDefault();
+    alertMessageSubmit();
+  });
+  
 });
 
 function alertMessageSubmit() {
-  alert("leaving"); // will change to modal
+  previouslyFocusedEl = document.activeElement;
+
+  modal.style.display = "block";
+  overlay.style.display = "block";
+
+  var focusableElsString =
+    'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, tabindex:not([tabindex="-1"])';
+  var focusableEls = modal.querySelectorAll(focusableElsString);
+
+  var Onlyfocusable = focusableEls[0];
+
+  Onlyfocusable.focus();
+
+  modal.addEventListener("keydown", trapFocus);
+
+  function trapFocus(e) {
+
+    if (e.keyCode == 9) {
+      if (e.shiftKey) {
+        if (document.activeElement === Onlyfocusable) {
+          event.preventDefault();
+          Onlyfocusable.focus();
+        }
+      } else {
+        if (document.activeElement === Onlyfocusable) {
+          event.preventDefault();
+          Onlyfocusable.focus();
+        }
+      }
+    }
+
+    if (e.keyCode == 27) closeModal();
+  }
+
+}
+function closeModal() {
+  modal.style.display = "none";
+  overlay.style.display = "none";
+
+  previouslyFocusedEl.focus();
 }
