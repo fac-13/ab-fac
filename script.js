72e83542a0214c9b16c99ab9b32c6cad65bcff6d
vlb834 <32312712+vlb834@users.noreply.github.com>
2018-03-01 11:56:58 +0000

    add aria labels/leave page alert to contact form relates to #10

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Write your code below only:
-<<<<<<< HEAD
-=======
   const submit = document.getElementById("send");
-  function alertSent() {
-    submit.style.display = "none";
-  }
 
-  submit.addEventListener("click", alertSent, false);
+  submit.addEventListener("click", alertMessageSubmit, false);
 
->>>>>>> master
   // Don't delete below this comment. Wrtie your code above!
 });
+
+function alertMessageSubmit() {
+  alert("leaving"); // will change to modal
+}
